8506d4a18ab7614c6f2054d7ed068fc252e71a4e
Kankayya Bura <ravi.kankayya-bura@capgemini.com>
2023-02-17 17:04:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+     <Header/>
+     {/* Header */}
+     {/* Header */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBDRZAC0OYltkGD4rJnfexrAgWd9_ZDTRw",
+    authDomain: "challenge-94566.firebaseapp.com",
+    projectId: "challenge-94566",
+    storageBucket: "challenge-94566.appspot.com",
+    messagingSenderId: "116173621420",
+    appId: "1:116173621420:web:13f043846ea9534b84b709",
+    measurementId: "G-3M8YBM4M4N"
+  };
